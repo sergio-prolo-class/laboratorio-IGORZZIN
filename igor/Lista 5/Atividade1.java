8d0f6935b0f86d626d56936833aa266cb28872ba
IGORZZIN <fernandoigor119@gmail.com>
2025-10-16 19:59:46 +0000

    Atividade 1 feita

--- igor/Lista 5/Atividade1.java
+++ igor/Lista 5/Atividade1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Escreve {
+public class Atividade1 {
     public static void main(String[] args) { 
       Scanner sc = new Scanner(System.in);
       System.out.print("Digite a temeratura em fahrenheit :");
