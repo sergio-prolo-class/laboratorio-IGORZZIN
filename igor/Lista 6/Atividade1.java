0d1de317ee5b713c37696350d7f1248dab0fcaf1
IGORZZIN <fernandoigor119@gmail.com>
2025-10-17 00:43:17 +0000

    Atividade 1 feita

---
+++ igor/Lista 6/Atividade1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Atividade1 {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    
+    // constantes 
+    double salario = 2000;
+    double comissao = 0.0;
+    double bonus = 0.0;
+
+    System.out.print( "Diga o total das vendas (R$): " );
+    double vendas = entrada.nextInt();
+
+    System.out.print( "Diga a avaliação do desempenho (1-10): ");
+    int avaliaçao = entrada.nextInt();
+
+    if (vendas >= 10000) {
+        comissao = vendas * 7.0;
+    }   else { comissao = vendas * 5.0;}
+
+     // Verifica se tem bônus
+        if (avaliaçao >= 8) 
+            {bonus = 500.00;}
+        
+
+        // Remuneração total
+        double total = salario + comissao + bonus;
+
+        // Saída formatada
+        System.out.println("Remuneração: R$ " + String.format("%.2f", total));
+
+        entrada.close();
+    }
+}
